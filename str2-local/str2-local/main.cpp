ae4977d1df10120e7eba4e002619268c30a52de6
zhuyizhi <zhouxiao1990@gmail.com>
2013-10-22 11:25:50 +0800

    commit from mac

---
+++ str2-local/str2-local/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  str2-local
+//
+//  Created by xiazhou on 10/22/13.
+//  Copyright (c) 2013 xiazhou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
